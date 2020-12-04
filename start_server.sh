f093c34cb088069a78903da86c1408dc81accb50
JonathanGrant <jonathanallengrant@gmail.com>
2020-12-04 11:08:12 -0500

    Init env and change starting script

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-docker run --rm -it -p 5000:500 -v $(pwd):/app word2vec
+set -ex
+
+#docker run --rm -it -p 5000:500 -v $(pwd):/app word2vec
+
+mkdir -p ~/tmp/
+tmpdir=$(mktemp -d ~/tmp/)
+
+# Step 1, clone letsencrypt as a wrapping directory
+git clone https://github.com/smallwat3r/docker-nginx-gunicorn-flask-letsencrypt.git "$tmpdir/docker-wrapper"
+
+# Step 2, add this repo to above
+mkdir -p "$tmpdir/docker-wrapper/src"
+git clone https://github.com/JonathanGrant/WordEmbeddingsServer "$tmpdir/docker-wrapper/src"
+
+# Step 3, copy .env file to wrapper
+cp "tmpdir/docker-wrapper/src/.env" "tmpdir/docker-wrapper/.env"
+
+# Step 4, launch wrapping docker container
+make dc-start
+
